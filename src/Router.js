615eade72e03e28d703ea17fbc33bd0ec48be34e
Dante6327 <jtgk6327@hanmail.net>
2023-03-06 17:07:42 +0900

    ADD : 초기 셋팅 진행

---
+++ src/Router.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/*<Route path="/" element={<Login />} />*/}
+        {/*<Route path="/main" element={<Main />} />*/}
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
