977414efcd305f8ce8be12b1d62fa51c0705ac6c
Dante6327 <jtgk6327@hanmail.net>
2023-03-08 16:15:14 +0900

    ADD : main 컴포넌트 슬라이드 구현

---
+++ src/pages/Main/ImgSlide/ImgSlide.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import leftArrow from '../../../assets/images/slide/left-arrow.png';
+import rightArrow from '../../../assets/images/slide/right-arrow.png';
+import './ImgSlide.scss';
+
+const ImgSlide = () => {
+  const slideRef = useRef(null);
+  const [currentImgOrder, setCurrentImgOrder] = useState(0);
+  const [imgWidth, setImgWidth] = useState(0);
+  const [slideData, setSlideData] = useState([]);
+  const slideRange = currentImgOrder * imgWidth;
+
+  useEffect(() => {
+    setImgWidth(slideRef.current.offsetWidth);
+  }, [imgWidth]);
+
+  useEffect(() => {
+    fetch('/data/slideData.json', { method: 'GET' })
+      .then(res => res.json())
+      .then(data => setSlideData(data));
+  }, []);
+
+  useEffect(() => {
+    slideRef.current.style.transition = 'all 0.5s ease-in-out';
+    slideRef.current.style.transform = `translateX(-${slideRange}px)`;
+  }, [currentImgOrder, slideRange, imgWidth]);
+
+  const moveToNextSlide = () => {
+    if (currentImgOrder === slideData.length - 1) return;
+    setCurrentImgOrder(currentImgOrder + 1);
+  };
+
+  const moveToPrevSlide = () => {
+    if (currentImgOrder === 0) return;
+    setCurrentImgOrder(currentImgOrder - 1);
+  };
+  return (
+    <div className="imgslide">
+      <div className="slideWrapper" ref={slideRef}>
+        {slideData.map(item => (
+          <img key={item.id} src={`${IMG_PATH}/${item.imgSrc}`} alt="pillImg" />
+        ))}
+      </div>
+
+      <button onClick={moveToPrevSlide}>
+        <img className="leftArrow" src={leftArrow} alt="leftImg" />
+      </button>
+
+      <button onClick={moveToNextSlide}>
+        <img className="rightArrow" src={rightArrow} alt="rightImg" />
+      </button>
+    </div>
+  );
+};
+
+export default ImgSlide;
+
+const IMG_PATH = 'images/slide';
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import ImgSlide from './ImgSlide/ImgSlide';
+import './Main.scss';
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div className="main">
+      <ImgSlide />
+    </div>
+  );
 };
 
 export default Main;
