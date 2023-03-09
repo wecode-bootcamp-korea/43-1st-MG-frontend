25a431cc68ba9313c8a80c69477a0d3562b33313
Dante6327 <jtgk6327@hanmail.net>
2023-03-09 20:50:27 +0900

    MODIFY : 충돌 재수정

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -5,7 +5,38 @@ import ProductList from './ProductList/ProductList';
 import './Main.scss';
 
 const Main = () => {
-  return <div>Main</div>;
+  const [productsData, setProductsData] = useState([]);
+  const params = useParams();
+  const cateId = Number(params.id);
+
+  useEffect(() => {
+    fetch('/data/productsData.json', {
+      method: 'GET',
+      headers: { 'Content-type': 'application/json' },
+    })
+      .then(res => {
+        if (res.ok) return res.json();
+        else throw new Error('통신 에러 발생 !');
+      })
+      .then(data => setProductsData(data))
+      .catch(err => alert(err));
+  }, []);
+
+  const filteredProducts = productsData.filter(product => {
+    if (cateId) return product.category_id === cateId;
+    else return productsData;
+  });
+
+  return (
+    <article className="main">
+      <section className="slide">
+        <ImgSlide />
+      </section>
+      <section className="products">
+        <ProductList cateId={cateId} filteredProducts={filteredProducts} />
+      </section>
+    </article>
+  );
 };
 
 export default Main;
