75e335ae1535946f2f08678a8f950260f01faad5
eunbinmoon <bin1109a@gmail.com>
2023-03-10 21:33:47 +0900

    ADD: 장바구니 UI 구현

--- src/pages/ShoppingBasket/ShoppingBasket.js
+++ src/pages/ShoppingBasket/ShoppingBasket.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+/* eslint-disable react/jsx-key */
+import React, { useEffect, useState } from 'react';
 import './ShoppingBasket.scss';
 import cartImg from '../../assets/images/shopping-cart.png';
 import { Product } from './component/Product';
@@ -6,13 +7,15 @@ import { Product } from './component/Product';
 const ShoppingBasket = () => {
   const [data, setData] = useState([]);
 
-  fetch('/data/data.json', {
-    method: 'GET',
-  })
-    .then(res => res.json())
-    .then(data => {
-      setData(data);
-    }, []);
+  useEffect(() => {
+    fetch('/data/data.json', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(data => {
+        setData(data);
+      });
+  }, []);
 
   return (
     <div className="cartContainer">
@@ -23,7 +26,9 @@ const ShoppingBasket = () => {
       <div className="cartlistContainer">
         <div className="cartChoose">
           <input type="checkbox" />
-          <span className="cartChooseAllCheckText">전체선택(2/2)</span>
+          <span className="cartChooseAllCheckText">
+            전체선택(2/{data.length})
+          </span>
           <span className="cartBar">|</span>
           <button className="deliteButton">선택삭제</button>
         </div>
@@ -31,10 +36,14 @@ const ShoppingBasket = () => {
           <span>일반배송</span>
         </div>
         <div className="cartProduct">
-          <Product data={data} setData={setData} />
+          {data.map(data => {
+            return <Product data={data} setData={setData} />;
+          })}
+
           <div className="cartTotalPrice">
             <div className="cartTotalPriceText">
-              <span>제품가격</span> <span> 123123원</span>
+              <span>제품가격</span>
+              <span> 123123원</span>
               <span>+ 배송비 3,000원 = </span> <span>123123132원</span>
             </div>
           </div>
--- src/pages/ShoppingBasket/component/Product.js
+++ src/pages/ShoppingBasket/component/Product.js
@@ -2,9 +2,7 @@ import React, { useState } from 'react';
 import closeImg from '../../../assets/images/cross.png';
 import './Product.scss';
 
-export const Product = props => {
-  const { data, setData } = props;
-  // console.log('p', props);
+export const Product = ({ data, setData }) => {
   const [count, setCount] = useState(1);
   if (count < 1) {
     setCount(1);
@@ -15,7 +13,7 @@ export const Product = props => {
       <input className="inCartproductCheck" type="checkbox" />
       <img src="./images/github.png" alt="productImage" />
       <div product className="inCartProductName">
-        <span className="productNameText">활력충전 멀티비타민</span>
+        <span className="productNameText">{data.productName}</span>
         <span className="productInfo">{count}개</span>
       </div>
       <div className="count">
@@ -37,7 +35,9 @@ export const Product = props => {
           </button>
         </div>
       </div>
-      <div className="productAllprice">4,900원</div>
+      <div className="productAllprice">
+        {data.productPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원
+      </div>
       <img className="productdelite" src={closeImg} alt="삭제버튼" />
     </div>
   );
