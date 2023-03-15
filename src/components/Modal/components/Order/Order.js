b09097cd7e1fd54783122ce0645e6b606cea7bf8
eunbinmoon <bin1109a@gmail.com>
2023-03-15 13:55:53 +0900

    중간저장

--- src/components/Modal/components/Order/Order.js
+++ src/components/Modal/components/Order/Order.js
@@ -1,23 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Order.scss';
 
 const Order = () => {
+  const [cartList, setCartList] = useState([]);
+
+  useEffect(() => {
+    fetch('/data/data.json')
+      .then(res => res.json())
+      .then(data => {
+        setCartList(data);
+      });
+  }, []);
+
+  const onclick = () => {
+    fetch('http://10.58.52.215:3000/users/signup', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json;charset=utf-8' },
+      body: JSON.stringify({ productID: 123, quentity: 5 }),
+    })
+      .then(response => response.json())
+      //응답 alert 띄우기
+      .then(data => console.log(data));
+  };
+
   return (
     <div className="order">
       <div className="orderListMain">주문상품 정보 / 총 3개</div>
       <div className="sendproduct">
-        <div>
+        {/* {cartList.map(item => {
+          return ( */}
+        <div className="cartListMap">
+          <div className="cartListProductMap">상품명:1</div>
+          <div className="cartListQuentityMap">수량:1</div>
+        </div>
+        <div className="cartListMap">
           <div>상품명:1</div>
           <div>수량:1</div>
         </div>
-        <div>
+        <div className="cartListMap">
           <div>상품명:1</div>
           <div>수량:1</div>
         </div>
-        <div>
-          <div>상품명:1</div>
-          <div>수량:1</div>
-        </div>
+        {/* //   );
+        // })} */}
       </div>
       <div className="orderListInfo">
         <div className="orderListInfoKey">
@@ -33,7 +58,7 @@ const Order = () => {
           <div className="lastpriceValue">150,000원</div>
         </div>
       </div>
-      <button>구매하기</button>
+      <button onClick={onclick}>구매하기</button>
     </div>
   );
 };
