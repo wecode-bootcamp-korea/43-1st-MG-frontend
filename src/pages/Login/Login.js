a795b1e2a5690808e3f26347142f1e647ae374b8
Dante6327 <jtgk6327@hanmail.net>
2023-03-16 09:52:09 +0900

    MODIFY : 로그인 잘못 푸시하여 원복

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,45 +1,29 @@
 import React, { useState } from 'react';
 import './Login.scss';
 import { useNavigate } from 'react-router-dom';
+
 const Login = () => {
   const navigate = useNavigate();
   const [inputValue, setInputValue] = useState({
-    email: '',
-    password: '',
+    id: '',
+    pw: '',
   });
-  const { email, password } = inputValue;
+  const { id, pw } = inputValue;
+
   const handleInput = e => {
     const { name, value } = e.target;
-    setInputValue({ ...inputValue, [name]: value });
+    setInputValue(prev => ({ ...prev, [name]: value }));
   };
-  const isValid = email.includes('@' && '.com') && password.length >= 5;
+  const isValid = id.includes('@' && '.com') && pw.length >= 5;
+
   const goToMain = () => {
-    console.log(email, password);
     if (isValid) {
-      fetch('http://10.58.52.209:3000/users/signin', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json;charset=utf-8', //필수로 넣어야함
-        },
-        body: JSON.stringify({
-          // 자바스크립트를 json 형태로 보내는 것
-          email: inputValue.email,
-          password: inputValue.password,
-        }),
-      }) //요청
-        .then(response => response.json())
-        // .then(data => console.log(data));
-        .then(response => {
-          console.log(response);
-          if (response.accessToken) {
-            localStorage.setItem('login-token', response.accessToken);
-          }
-        });
       navigate('/main');
     } else {
       alert('로그인 정보를 올바르게 입력하세요');
     }
   };
+
   return (
     <div className="login">
       <div className="loginWelcome">
@@ -51,8 +35,8 @@ const Login = () => {
             className="loginMainInputId"
             type="text"
             placeholder="이메일을 입력해주세요"
-            name="email"
-            value={email}
+            name="id"
+            value={id}
             onChange={handleInput}
           />
           <br />
@@ -60,15 +44,15 @@ const Login = () => {
             className="loginMainInputPw"
             type="password"
             placeholder="비밀번호를 입력해주세요"
-            name="password"
-            value={password}
+            name="pw"
+            value={pw}
             onChange={handleInput}
           />
         </div>
         <div>
           <button
             className={isValid ? 'buttonLogin' : 'buttonLoginDisabled'}
-            disabled={email === '' || password === '' ? true : false}
+            disabled={id === '' || pw === '' ? true : false}
             onClick={goToMain}
           >
             로그인
